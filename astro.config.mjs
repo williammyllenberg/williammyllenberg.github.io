c991020ff350ede545eac07851388da73a2f921d
William Myllenberg <william@MacBook-Pro.local>
2024-05-22 21:39:36 +0200

    add redirect for old utf-8 (åäö) links

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,10 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://movingartdance.se',
+	redirects: {
+		'https://movingartdance.se/anmälan/höganäs/': 'https://movingartdance.se/anmalan/hoganas/',
+		'https://movingartdance.se/anmälan/åstorp/': 'https://movingartdance.se/anmalan/astorp/'
+	},
 	integrations: [tailwind(), sitemap({
 		filter: (page) => page !== 'https://movingartdance.se/anmald/'
 	})]
