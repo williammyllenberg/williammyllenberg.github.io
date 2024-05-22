f0f78c467bf3d22297421481cb5829272dbf4342
William Myllenberg <william@MacBook-Pro.local>
2024-05-22 21:40:31 +0200

    redirect fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
 	site: 'https://movingartdance.se',
 	redirects: {
-		'https://movingartdance.se/anmälan/höganäs/': 'https://movingartdance.se/anmalan/hoganas/',
-		'https://movingartdance.se/anmälan/åstorp/': 'https://movingartdance.se/anmalan/astorp/'
+		'/anmälan/höganäs': '/anmalan/hoganas',
+		'/anmälan/åstorp': '/anmalan/astorp'
 	},
 	integrations: [tailwind(), sitemap({
 		filter: (page) => page !== 'https://movingartdance.se/anmald/'
