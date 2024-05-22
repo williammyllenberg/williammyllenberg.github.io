d50e8337d319059fb457b0ee1d745e957875d5eb
William Myllenberg <willmyllenberg@gmail.com>
2024-05-22 15:47:36 +0200

    add 404, add meta desc for SEO, improve some text

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://movingartdance.se',
-	integrations: [tailwind(), sitemap()]
+	integrations: [tailwind(), sitemap({
+		filter: (page) => page !== 'https://movingartdance.se/anmald/'
+	})]
 });
