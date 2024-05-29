60ccd8339586efb92c9ce379d04412f038df9d28
William Myllenberg <william@MacBook-Pro.local>
2024-05-29 18:23:28 +0200

    updates for astorp and fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://movingartdance.se',
-	redirects: {
-		'/anmälan/höganäs': '/anmalan/hoganas',
-		'/anmälan/åstorp': '/anmalan/astorp'
-	},
-	integrations: [tailwind(), sitemap({
-		filter: (page) => page !== 'https://movingartdance.se/anmald/'
-	})]
+  site: 'https://movingartdance.se',
+  redirects: {
+    '/anmälan/höganäs': '/anmalan/hoganas',
+    '/anmälan/åstorp': '/anmalan/astorp'
+  },
+  integrations: [tailwind(), sitemap({
+    filter: page => page !== 'https://movingartdance.se/anmald/'
+  }), icon()]
 });
